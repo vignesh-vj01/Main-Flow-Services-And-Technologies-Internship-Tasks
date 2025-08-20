8ce7fdb62d50732e2c3fb95cd5eba556233d238b
vignesh-vj01 <vigneshraj3011.com>
2025-08-20 12:30:46 +0530

    Resolved merge conflicts

--- Task 6/ConnectionProvider.java
+++ Task 6/ConnectionProvider.java
@@ -1,12 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+/* Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Project;
 import java.sql.*;
 /**
  *
- * @author hinab
+ * @author vicky
  */
 public class ConnectionProvider {
     public static Connection getCon()
